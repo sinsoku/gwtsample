e2773b1fae81d329854b42c5aaf74a364a2a9458
sinsoku <sinsoku.listy@gmail.com>
2013-01-23 01:19:46 +0900

    mvn package

---
+++ target/.generated/sample/sinsoku/client/Messages_.java
@@ -0,0 +1,12 @@
+package sample.sinsoku.client;
+
+public class Messages_ implements sample.sinsoku.client.Messages {
+  
+  public java.lang.String nameField() {
+    return "Enter your name";
+  }
+  
+  public java.lang.String sendButton() {
+    return "Send";
+  }
+}
