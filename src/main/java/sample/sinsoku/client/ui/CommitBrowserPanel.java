c131d685af75c3a02c26d90a94853009a2a3283b
sinsoku <sinsoku.listy@gmail.com>
2013-03-17 22:35:31 +0900

    canvasの領域のデザイン調整

--- src/main/java/sample/sinsoku/client/ui/CommitBrowserPanel.java
+++ src/main/java/sample/sinsoku/client/ui/CommitBrowserPanel.java
@@ -6,7 +6,7 @@ import com.google.gwt.user.client.ui.FlowPanel;
 public class CommitBrowserPanel extends FlowPanel {
 
   public CommitBrowserPanel() {
-    addStyleName("top-commit-browse");
+    addStyleName("commit-browser");
     
     Canvas canvas = Canvas.createIfSupported();
     add(canvas);
